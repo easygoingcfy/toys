02e741efb591d9ee5264da54339510b36941e91c
caofy <caofy@antwork.link>
2024-02-01 19:49:30 +0800

    to: add longitude and latitude transform window

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import datetime
 import re
 import sys
 import traceback
+from enum import Enum
 
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
+from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow, QComboBox,
                              QMessageBox, QPushButton, QTextEdit, QVBoxLayout,
                              QWidget)
 from PyQt5.QtGui import QIcon
@@ -120,8 +121,153 @@ class TimeTransform(QWidget):
         time_str = timestamp2str(int(self.timestamp_line.text()))
         self.datetime_line.setText(time_str)
 
+class longitude_latitude_transform(QWidget):
+    """经纬度转换
+
+    Args:
+        QWidget (_type_): _description_
+    """
+    def __init__(self):
+        super().__init__()
+        self.direction = None
+        self.init_ui()
+        sys._excepthook = sys.excepthook
+        sys.excepthook = self.exception_hook
+
+    def exception_hook(self, exctype, value, traceback_info):
+        """捕获异常并弹窗显示
+
+        Args:
+            exctype (_type_): 异常类型
+            value (_type_): 异常值
+            traceback (_type_): 异常的traceback
+        """
+        traceback_format = traceback.format_exception(exctype, value,
+                                                      traceback_info)
+        traceback_string = "".join(traceback_format)
+        QMessageBox.critical(None, "Error", traceback_string)
+        self._excepthook(exctype, value, traceback_info)
+
+    def init_ui(self):
+        #显示栏
+        # prompt
+        self.longitude_prompt = QLabel("经度", self)
+        self.latitude_prompt = QLabel("纬度", self)
+        self.direction_prompt = QLabel("方向", self)
+        self.distance_prompt = QLabel("距离", self)
+
+        # edit line
+        self.longitude_line = QLineEdit(self)
+        self.latitude_line = QLineEdit(self)
+        self.direction_combo = QComboBox(self)
+        self.direction_combo.addItem("东")
+        self.direction_combo.addItem("南")
+        self.direction_combo.addItem("西")
+        self.direction_combo.addItem("北")
+        self.direction_combo.activated[str].connect(self.dir_combo_on_activated)
+        self.distance_line = QLineEdit(self)
+
+        self.longitude_result_line = QLineEdit(self)
+        self.longitude_result_line.setReadOnly(True)
+        self.latitude_result_line = QLineEdit(self)
+        self.latitude_result_line.setReadOnly(True)
+        #self.longitude_result_line.setText(self.ts_unit.value)
+        #按钮
+        cal_button = QPushButton("计算")
+        cal_button.clicked.connect(self.cal_button_implement)
+        # ts_button = QPushButton("开始时间戳")
+        # ts_button.clicked.connect(lambda: self.ts2date(self.ts_line.text()))
+        # te_button = QPushButton("结束时间戳")
+        # te_button.clicked.connect(lambda: self.ts2date(self.te_line.text()))
+        # unit_button = QPushButton("单位")
+        # unit_button.clicked.connect(self.unit_transform)
+        hbox_prompt_line = generate_hbox([
+            self.longitude_prompt,
+            self.latitude_prompt,
+            self.direction_prompt,
+            self.distance_prompt,
+        ])
+        hbox_edit_line = generate_hbox([
+            self.longitude_line,
+            self.latitude_line,
+            self.distance_line,
+            self.direction_combo,
+        ])
+        hbox_buttion_line = generate_hbox(
+            [cal_button])
+        result_line = generate_hbox([
+            self.longitude_result_line,
+            self.latitude_result_line
+        ])
+        #时间变换行: 详细数据的时间戳转换 fmt:%Y-%m-%d %H:%M:%S
+        # self.datetime_line = QLineEditDate(self)
+        # self.timestamp_line = QLineEditTs(self, self.ts_unit)
+
+        # datetime_button = QPushButton("时间")
+        # datetime_button.clicked.connect(self.datetime_button_implement)
+        # timestamp_button = QPushButton("时间戳")
+        # timestamp_button.clicked.connect(self.timestamp_button_implement)
+        # hbox_time_transform_line = generate_hbox(
+        #     [self.datetime_line, self.timestamp_line])
+        # hbox_time_transform_button = generate_hbox(
+        #     [datetime_button, timestamp_button])
+
+        #set layout
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        box_add_layout(vbox, [
+            hbox_prompt_line,
+            hbox_edit_line,
+            hbox_buttion_line,
+            result_line,
+            #hbox_time_transform_button,
+            #hbox_time_transform_line
+        ])
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('toys')
+        self.setWindowIcon(QIcon('source/image/cat.jpg'))
+        self.show()
+    
+    def cal_button_implement(self):
+        print("cal button")
+    
+    def dir_combo_on_activated(self, text):
+        if (text == "东"):
+            self.direction = Direction.East
+        if (text == "南"):
+            self.direction = Direction.South
+        if (text == "西"):
+            self.direction = Direction.West
+        if (text == "北"):
+            self.direction = Direction.North
+
+
+def cal_lng(lng, lat, distance, dir):
+    interval = 0.000001 # °
+    gap = 1.    # 1 meters
+    degrees = distance / gap * interval
+    degrees = degrees if dir == Direction.East else (-1 * degrees)
+    return lng + degrees, lat
+
+def cal_lat(lng, lat, distance, dir):
+    interval = 0.000001 # °
+    gap = 1.1    # 1.1 meters
+    degrees = distance / gap * interval
+    degrees = degrees if dir == Direction.North else (-1 * degrees)
+    return lng, lat + degrees
+
+class Direction(Enum):
+    North =1
+    South = 2
+    East = 3
+    West = 4
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    time_tf = TimeTransform()
+    #time_tf = TimeTransform()
+    tf = longitude_latitude_transform()
     sys.exit(app.exec_())
