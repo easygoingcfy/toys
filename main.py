85c474aa8984bbf3570010fbf632ea7f4a6d46c1
easygoingcfy <caofangyu1997@163.com>
2024-02-02 00:07:00 +0800

    feat: add longitude and latitude transformer

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import datetime
 import re
 import sys
+import math
 import traceback
 from enum import Enum
 
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow, QComboBox,
-                             QMessageBox, QPushButton, QTextEdit, QVBoxLayout,
-                             QWidget)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QComboBox,
+    QMessageBox,
+    QPushButton,
+    QTextEdit,
+    QVBoxLayout,
+    QWidget,
+)
 from PyQt5.QtGui import QIcon
 
-from model.refactor import QLineEdit, QLineEditTs, TsUnit, QLineEditDate
+from model.refactor import QLineEdit, QLineEditTs, TsUnit, QLineEditDate, QLineEditFloat
 from tools import box_add_layout, generate_hbox
-from utils.time_util import (extract_date, extract_time, str2timestamp,
-                             timestamp2str, ts_range_by_date)
+from utils.time_util import (
+    extract_date,
+    extract_time,
+    str2timestamp,
+    timestamp2str,
+    ts_range_by_date,
+)
 
 
 class TimeTransform(QWidget):
-    """实现日期字符串与时间戳之间的转换
-    """
+    """实现日期字符串与时间戳之间的转换"""
 
     def __init__(self):
         super().__init__()
@@ -34,22 +48,21 @@ class TimeTransform(QWidget):
             value (_type_): 异常值
             traceback (_type_): 异常的traceback
         """
-        traceback_format = traceback.format_exception(exctype, value,
-                                                      traceback_info)
+        traceback_format = traceback.format_exception(exctype, value, traceback_info)
         traceback_string = "".join(traceback_format)
         QMessageBox.critical(None, "Error", traceback_string)
         self._excepthook(exctype, value, traceback_info)
 
     def init_ui(self):
         """时间变换行:将日期数据与时间戳相互转换 fmt:%Y-%m-%d"""
-        #显示栏
+        # 显示栏
         self.date_str_line = QLineEditDate(self)
         self.ts_line = QLineEditTs(self, self.ts_unit)
         self.te_line = QLineEditTs(self, self.ts_unit)
         self.unit_line = QLineEdit(self)
         self.unit_line.setReadOnly(True)
         self.unit_line.setText(self.ts_unit.value)
-        #按钮
+        # 按钮
         date_button = QPushButton("日期")
         date_button.clicked.connect(self.date_button_implement)
         ts_button = QPushButton("开始时间戳")
@@ -59,10 +72,12 @@ class TimeTransform(QWidget):
         unit_button = QPushButton("单位")
         unit_button.clicked.connect(self.unit_transform)
         hbox_edit_line = generate_hbox(
-            [self.date_str_line, self.ts_line, self.te_line, self.unit_line])
+            [self.date_str_line, self.ts_line, self.te_line, self.unit_line]
+        )
         hbox_buttion_line = generate_hbox(
-            [date_button, ts_button, te_button, unit_button])
-        #时间变换行: 详细数据的时间戳转换 fmt:%Y-%m-%d %H:%M:%S
+            [date_button, ts_button, te_button, unit_button]
+        )
+        # 时间变换行: 详细数据的时间戳转换 fmt:%Y-%m-%d %H:%M:%S
         self.datetime_line = QLineEditDate(self)
         self.timestamp_line = QLineEditTs(self, self.ts_unit)
 
@@ -71,24 +86,29 @@ class TimeTransform(QWidget):
         timestamp_button = QPushButton("时间戳")
         timestamp_button.clicked.connect(self.timestamp_button_implement)
         hbox_time_transform_line = generate_hbox(
-            [self.datetime_line, self.timestamp_line])
-        hbox_time_transform_button = generate_hbox(
-            [datetime_button, timestamp_button])
+            [self.datetime_line, self.timestamp_line]
+        )
+        hbox_time_transform_button = generate_hbox([datetime_button, timestamp_button])
 
-        #set layout
+        # set layout
 
         vbox = QVBoxLayout()
         vbox.addStretch(1)
-        box_add_layout(vbox, [
-            hbox_buttion_line, hbox_edit_line, hbox_time_transform_button,
-            hbox_time_transform_line
-        ])
+        box_add_layout(
+            vbox,
+            [
+                hbox_buttion_line,
+                hbox_edit_line,
+                hbox_time_transform_button,
+                hbox_time_transform_line,
+            ],
+        )
         vbox.addStretch(1)
 
         self.setLayout(vbox)
         self.setGeometry(300, 300, 800, 600)
-        self.setWindowTitle('toys')
-        self.setWindowIcon(QIcon('source/image/cat.jpg'))
+        self.setWindowTitle("toys")
+        self.setWindowIcon(QIcon("source/image/cat.jpg"))
         self.show()
 
     def date_button_implement(self):
@@ -121,15 +141,16 @@ class TimeTransform(QWidget):
         time_str = timestamp2str(int(self.timestamp_line.text()))
         self.datetime_line.setText(time_str)
 
+
 class longitude_latitude_transform(QWidget):
     """经纬度转换
 
     Args:
         QWidget (_type_): _description_
     """
+
     def __init__(self):
         super().__init__()
-        self.direction = None
         self.init_ui()
         sys._excepthook = sys.excepthook
         sys.excepthook = self.exception_hook
@@ -142,64 +163,61 @@ class longitude_latitude_transform(QWidget):
             value (_type_): 异常值
             traceback (_type_): 异常的traceback
         """
-        traceback_format = traceback.format_exception(exctype, value,
-                                                      traceback_info)
+        traceback_format = traceback.format_exception(exctype, value, traceback_info)
         traceback_string = "".join(traceback_format)
         QMessageBox.critical(None, "Error", traceback_string)
         self._excepthook(exctype, value, traceback_info)
 
     def init_ui(self):
-        #显示栏
+        # 显示栏
         # prompt
         self.longitude_prompt = QLabel("经度", self)
         self.latitude_prompt = QLabel("纬度", self)
-        self.direction_prompt = QLabel("方向", self)
-        self.distance_prompt = QLabel("距离", self)
+        self.direction_prompt = QLabel("东西(东+)", self)
+        self.distance_prompt = QLabel("南北(北+)", self)
 
         # edit line
-        self.longitude_line = QLineEdit(self)
-        self.latitude_line = QLineEdit(self)
-        self.direction_combo = QComboBox(self)
-        self.direction_combo.addItem("东")
-        self.direction_combo.addItem("南")
-        self.direction_combo.addItem("西")
-        self.direction_combo.addItem("北")
-        self.direction_combo.activated[str].connect(self.dir_combo_on_activated)
-        self.distance_line = QLineEdit(self)
+        self.longitude_line = QLineEditFloat(self)
+        self.latitude_line = QLineEditFloat(self)
+        self.lng_distance_line = QLineEditFloat(self)
+        self.lat_distance_line = QLineEditFloat(self)
+        self.lng_distance_line.setText(0.0)
+        self.lat_distance_line.setText(0.0)
 
         self.longitude_result_line = QLineEdit(self)
         self.longitude_result_line.setReadOnly(True)
         self.latitude_result_line = QLineEdit(self)
         self.latitude_result_line.setReadOnly(True)
-        #self.longitude_result_line.setText(self.ts_unit.value)
-        #按钮
+
+        # 按钮
         cal_button = QPushButton("计算")
         cal_button.clicked.connect(self.cal_button_implement)
-        # ts_button = QPushButton("开始时间戳")
-        # ts_button.clicked.connect(lambda: self.ts2date(self.ts_line.text()))
-        # te_button = QPushButton("结束时间戳")
-        # te_button.clicked.connect(lambda: self.ts2date(self.te_line.text()))
-        # unit_button = QPushButton("单位")
-        # unit_button.clicked.connect(self.unit_transform)
-        hbox_prompt_line = generate_hbox([
-            self.longitude_prompt,
-            self.latitude_prompt,
-            self.direction_prompt,
-            self.distance_prompt,
-        ])
-        hbox_edit_line = generate_hbox([
-            self.longitude_line,
-            self.latitude_line,
-            self.distance_line,
-            self.direction_combo,
-        ])
-        hbox_buttion_line = generate_hbox(
-            [cal_button])
-        result_line = generate_hbox([
-            self.longitude_result_line,
-            self.latitude_result_line
-        ])
-        #时间变换行: 详细数据的时间戳转换 fmt:%Y-%m-%d %H:%M:%S
+        hbox_prompt_line = generate_hbox(
+            [
+                self.longitude_prompt,
+                self.latitude_prompt,
+                self.direction_prompt,
+                self.distance_prompt,
+            ]
+        )
+        hbox_edit_line = generate_hbox(
+            [
+                self.longitude_line,
+                self.latitude_line,
+                self.lng_distance_line,
+                self.lat_distance_line,
+            ]
+        )
+        hbox_button_line = generate_hbox(
+            [
+                cal_button,
+            ]
+        )
+        # hbox_button_line.addStretch(1)
+        result_line = generate_hbox(
+            [self.longitude_result_line, self.latitude_result_line]
+        )
+        # 时间变换行: 详细数据的时间戳转换 fmt:%Y-%m-%d %H:%M:%S
         # self.datetime_line = QLineEditDate(self)
         # self.timestamp_line = QLineEditTs(self, self.ts_unit)
 
@@ -212,62 +230,84 @@ class longitude_latitude_transform(QWidget):
         # hbox_time_transform_button = generate_hbox(
         #     [datetime_button, timestamp_button])
 
-        #set layout
+        # set layout
 
         vbox = QVBoxLayout()
         vbox.addStretch(1)
-        box_add_layout(vbox, [
-            hbox_prompt_line,
-            hbox_edit_line,
-            hbox_buttion_line,
-            result_line,
-            #hbox_time_transform_button,
-            #hbox_time_transform_line
-        ])
+        box_add_layout(
+            vbox,
+            [
+                hbox_prompt_line,
+                hbox_edit_line,
+                hbox_button_line,
+                result_line,
+                # hbox_time_transform_button,
+                # hbox_time_transform_line
+            ],
+        )
         vbox.addStretch(1)
 
         self.setLayout(vbox)
         self.setGeometry(300, 300, 800, 600)
-        self.setWindowTitle('toys')
-        self.setWindowIcon(QIcon('source/image/cat.jpg'))
+        self.setWindowTitle("toys")
+        self.setWindowIcon(QIcon("source/image/cat.jpg"))
         self.show()
-    
+
     def cal_button_implement(self):
+        lng = self.longitude_line.text()
+        lat = self.latitude_line.text()
         print("cal button")
+        lng_distance = self.lng_distance_line.text()
+        lat_distance = self.lat_distance_line.text()
+        distance = math.sqrt(lng_distance**2 + lat_distance**2) / 1000
+        bearing = self.calculate_bearing(lng_distance, lat_distance)
+        lat, lng = self.haversine(lat, lng, distance, bearing)
+        self.longitude_result_line.setText(lng)
+        self.latitude_result_line.setText(lat)
     
-    def dir_combo_on_activated(self, text):
-        if (text == "东"):
-            self.direction = Direction.East
-        if (text == "南"):
-            self.direction = Direction.South
-        if (text == "西"):
-            self.direction = Direction.West
-        if (text == "北"):
-            self.direction = Direction.North
 
+    def calculate_bearing(self, east, north):
+        # 使用反正切函数计算方向角
+        bearing_rad = math.atan2(east, north)
+        # 将弧度转换为角度
+        bearing_deg = math.degrees(bearing_rad)
+        # 调整角度范围为 0 到 360 度
+        bearing_deg = (bearing_deg + 360) % 360
 
-def cal_lng(lng, lat, distance, dir):
-    interval = 0.000001 # °
-    gap = 1.    # 1 meters
-    degrees = distance / gap * interval
-    degrees = degrees if dir == Direction.East else (-1 * degrees)
-    return lng + degrees, lat
+        return bearing_deg
 
-def cal_lat(lng, lat, distance, dir):
-    interval = 0.000001 # °
-    gap = 1.1    # 1.1 meters
-    degrees = distance / gap * interval
-    degrees = degrees if dir == Direction.North else (-1 * degrees)
-    return lng, lat + degrees
+    def haversine(self, lat1, lon1, distance, bearing):
+        # 地球半径，单位：千米
+        R = 6371.0
+        # 将角度转换为弧度
+        lat1_rad = math.radians(lat1)
+        lon1_rad = math.radians(lon1)
+        bearing_rad = math.radians(bearing)
+        # 计算新点的纬度
+        lat2_rad = math.asin(math.sin(lat1_rad) * math.cos(distance / R) +
+                            math.cos(lat1_rad) * math.sin(distance / R) * math.cos(bearing_rad))
+        # 计算新点的经度
+        lon2_rad = lon1_rad + math.atan2(math.sin(bearing_rad) * math.sin(distance / R) * math.cos(lat1_rad),
+                                        math.cos(distance / R) - math.sin(lat1_rad) * math.sin(lat2_rad))
+        # 将弧度转换为角度
+        lat2 = math.degrees(lat2_rad)
+        lon2 = math.degrees(lon2_rad)
 
-class Direction(Enum):
-    North =1
-    South = 2
-    East = 3
-    West = 4
+        return lat2, lon2
+
+def calculate_lng_and_lat(lng, lat, lng_distance, lat_distance):
+    lng_interval = 0.000001  # °
+    lng_gap = 1.0  # 1 meters
+    lat_interval = 0.000001  # °
+    lat_gap = 1.1  # 1.1 meters
+
+    lng_degree = lng_distance / lng_gap * lng_interval
+    lat_degree = lat_distance / lat_gap * lat_interval
+    return lng + lng_degree, lat + lat_degree
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    #time_tf = TimeTransform()
+    # time_tf = TimeTransform()
     tf = longitude_latitude_transform()
     sys.exit(app.exec_())
--- tools.py
+++ tools.py
@@ -2,21 +2,31 @@
 """
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
-                             QMainWindow, QPushButton, QTextEdit, QVBoxLayout,
-                             QWidget)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QTextEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 
-def generate_hbox(widgets: list):
+def generate_hbox(widgets: list, strech=1):
     hbox = QHBoxLayout()
     for widget in widgets:
         if hasattr(widget, "setAlignment"):
             widget.setAlignment(Qt.AlignCenter)
-        hbox.addWidget(widget)
+        hbox.addWidget(widget, strech)
     return hbox
 
 
 def box_add_layout(box, layouts: list):
     for layout in layouts:
-        box.addLayout(layout)
-
+        if isinstance(layout, QHBoxLayout):
+            box.addLayout(layout)
+        else:
+            box.addWidget(layout)
